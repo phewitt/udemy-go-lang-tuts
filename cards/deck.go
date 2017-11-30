1669f543378fc7f02dc68c9af7c19defe17aae83
Parker Hewitt <Hewittparker@gmail.com>
2017-11-29 20:01:26 -0700

    add reciever for deck

--- cards/deck.go
+++ cards/deck.go
@@ -1,5 +1,14 @@
 package main
 
+import "fmt"
+
 // Create a new type of 'deck'
 // which is a slice of strings
 type deck []string
+
+// receiver for type deck
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
