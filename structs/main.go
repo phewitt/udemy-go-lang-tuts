a6531a918a29f6e0dc867be63fbedda8bc885cef
Parker Hewitt <Hewittparker@gmail.com>
2017-12-01 13:10:07 -0700

    structs in structs

--- structs/main.go
+++ structs/main.go
@@ -2,14 +2,26 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
-	var p person
-	p.firstName = "Alex"
-	p.lastName = "Anderson"
+	p := person{
+		firstName: "Jim",
+		lastName:  "Frank",
+		contact: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 94000,
+		},
+	}
+
 	fmt.Printf("%+v\n", p)
 }
